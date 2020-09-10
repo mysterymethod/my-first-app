5bda5ddabba2b1137de18369d1b51178763e8ae1
mysterymethod <chandra.pranay@yahoo.in>
2020-09-10 15:00:04 +0530

    Complete Ch-3 Base Features

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    persons: [
+      { name: "Max", age: 30 },
+      { name: "Manu", age: 40 },
+      { name: "Seema", age: 18 }
+    ]
+  }
+
+  onClickHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 30 },
+        { name: "Manu", age: 40 },
+        { name: "Seema", age: 18 }
+      ]
+    })
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 30 },
+        { name: event.target.value, age: 40 },
+        { name: "Seema", age: 18 }
+      ]
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1> Hi I am React App </h1>
+        <button
+          onClick = {this.onClickHandler.bind(this, "Max-Bhai")}
+        > Switch Name </button>
+
+        <Person 
+          name = {this.state.persons[0].name} 
+          age = {this.state.persons[0].age}
+        />
+
+        <Person 
+          name = {this.state.persons[1].name} 
+          age = {this.state.persons[1].age}
+          change = {this.nameChangeHandler}
+        />
+
+
+
+
+        {/* {this.state.person.forEach((item, i)) => {
+          <Person 
+            name = item.name 
+            age = item.age
+          />
+        }} */}
+
+
+
+        {/* <Person name="Max" age="30"/>
+        <Person name="Manu" age="40"> My hobbies: Racing and Gaming. </Person>
+        <Person name="Sima" age="18"/> */}
+      </div>
+    )
+  }
 }
 
 export default App;
