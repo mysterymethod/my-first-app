348f1f7a72353b636849dc41fe594bfca309aa2d
mysterymethod <chandra.pranay@yahoo.in>
2020-09-10 18:06:53 +0530

    Complete working with list and conditionals

--- src/App.js
+++ src/App.js
@@ -6,67 +6,74 @@ class App extends Component {
 
   state = {
     persons: [
-      { name: "Max", age: 30 },
-      { name: "Manu", age: 40 },
-      { name: "Seema", age: 18 }
-    ]
+      { id: 1, name: "Max", age: 30 },
+      { id: 2, name: "Manu", age: 40 },
+      { id: 3, name: "Seema", age: 18 }
+    ],
+    showPerons: false
   }
 
-  onClickHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: 30 },
-        { name: "Manu", age: 40 },
-        { name: "Seema", age: 18 }
-      ]
-    })
+  onClickHandler = () => {
+    const doesShow = this.state.showPerons;
+    this.setState({showPerons: !doesShow})
   }
 
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: "Max", age: 30 },
-        { name: event.target.value, age: 40 },
-        { name: "Seema", age: 18 }
-      ]
-    })
+
+  nameChangeHandler = ( event, id ) => {
+
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons} );
   }
 
+  deletePerson = (index) => {
+    const persons = [...this.state.persons];
+    persons.splice(index,1);
+    this.setState({persons: persons});
+  }
 
   render() {
+
+    let persons = null;
+
+  if(this.state.showPerons) {
+    persons = (
+      <div>
+        {this.state.persons.map((item,i) => {
+          return (
+            <Person 
+              name = {item.name}
+              age = {item.age}
+              key = {item.id}
+              click = {this.deletePerson.bind(this,i)}
+              change = {(event) => this.nameChangeHandler(event, item.id)}
+            />
+          )
+        })}
+      </div>
+    )
+  }
+
     return (
       <div className="App">
         <h1> Hi I am React App </h1>
         <button
-          onClick = {this.onClickHandler.bind(this, "Max-Bhai")}
-        > Switch Name </button>
+          onClick = {this.onClickHandler}
+        > Toggle Persons </button>
 
-        <Person 
-          name = {this.state.persons[0].name} 
-          age = {this.state.persons[0].age}
-        />
-
-        <Person 
-          name = {this.state.persons[1].name} 
-          age = {this.state.persons[1].age}
-          change = {this.nameChangeHandler}
-        />
-
-
-
-
-        {/* {this.state.person.forEach((item, i)) => {
-          <Person 
-            name = item.name 
-            age = item.age
-          />
-        }} */}
-
-
-
-        {/* <Person name="Max" age="30"/>
-        <Person name="Manu" age="40"> My hobbies: Racing and Gaming. </Person>
-        <Person name="Sima" age="18"/> */}
+        {persons}
+        
       </div>
     )
   }
